3eef768443e9603c854abefed00b55296877f2c1
ShantanuNimbalkar <87299589+ShantanuNimbalkar@users.noreply.github.com>
2023-12-07 16:12:26 +0530

    Update ArrayShouldBePrivate.java

--- ArrayShouldBePrivate.java
+++ ArrayShouldBePrivate.java
@@ -10,5 +10,6 @@ public class ArrayShouldBePrivate{
   private static final int arr3[]={10,20,30,40};
   
   protected static final int arr4[]={123,345,678,901};
+  public static final int[] arr10={1,2,3,4,5,56};
   
 }
